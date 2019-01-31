dbe746d3345c17367e2cf6ec738327779245db2e
Alexander Kolovatov <a.kolovatov@gmail.com>
2019-01-31 18:51:50 +0600

    initial commit

--- PaintApp/Controllers/ViewController.swift
+++ PaintApp/Controllers/ViewController.swift
@@ -10,11 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let canvas = Canvas()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+     
+        view.addSubview(canvas)
+        canvas.frame = view.frame
+        canvas.backgroundColor = .white
     }
 
 
 }
+
 
---
+++ PaintApp/Models/Line.swift
@@ -0,0 +1,9 @@
+//
+//  Line.swift
+//  PaintApp
+//
+//  Created by Alex Kolovatov on 31/01/2019.
+//  Copyright © 2019 Alex Kolovatov. All rights reserved.
+//
+
+import Foundation
